b3ca7622dd32fce2c8c5aa5c53ddbbb66ae79b9c
reznychenkooryna <reznychenkooryna@gmail.com>
2025-10-01 22:13:57 +0300

    Update Lab_04_1.cpp

--- Lab_04_1/Lab_04_1.cpp
+++ Lab_04_1/Lab_04_1.cpp
@@ -12,7 +12,7 @@ int main() {
     S = 0;
     i = k;
     while (i <= 15) {
-        S += cos(1. * i) / (1 + pow(sin(1.0 * i), 2));
+        S += cos(1. * i) / (1 + pow(sin(1. * i), 2));
         i++;
     }
     cout << S << endl;
@@ -20,7 +20,7 @@ int main() {
     S = 0;
     i = k;
     do {
-        S += cos(1. * i) / (1 + pow(sin(1.0 * i), 2));
+        S += cos(1. * i) / (1 + pow(sin(1. * i), 2));
         i++;
     } while (i <= 15);
     cout << S << endl;
@@ -28,13 +28,13 @@ int main() {
     S = 0;
     for (i = k; i <= 15; i++)
     {
-        S += cos(1. * i) / (1 + pow(sin(1.0 * i), 2));
+        S += cos(1. * i) / (1 + pow(sin(1. * i), 2));
     }
     cout << S << endl;
     S = 0;
     for (i = 15; i >= k; i--)
     {
-        S += cos(1. * i) / (1 + pow(sin(1.0 * i), 2));
+        S += cos(1. * i) / (1 + pow(sin(1. * i), 2));
     }
     cout << S << endl;
 
